ab9264439e8838e69307ff315ee5f0f1455ed04b
alanassell <mistermox99@gmail.com>
2023-11-13 23:16:57 -0300

    commit 13-11-23

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://192.168.0.10:8080/',    
-    baseURL: 'http://190.17.1.92:8080/',
+    baseURL: 'https://190.17.1.92:8080/',
     // baseURL: 'http://192.168.2.102:8080/',
 });
 
